50bf748e029494f428aa99818bd1f009df02ba6a
Parsa Enami <p.enaami@digikala.com>
2021-04-01 12:54:26 +0430

    trying to fix heroku...

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ if (module.hot) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
